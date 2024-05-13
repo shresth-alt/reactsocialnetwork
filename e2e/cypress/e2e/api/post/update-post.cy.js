7a5d652de86917e9e532c025add7eb28c88a462d
Shresth Khandelwal <83925559+shresth-alt@users.noreply.github.com>
2024-05-13 14:15:14 +0530

    Update profile and post image URLs
    - Updated profile image URL to use a new image.
    - Updated post image URL to use a different image.
    
    These changes ensure that the test uses the latest images for profile and post.

--- e2e/cypress/e2e/api/post/update-post.cy.js
+++ e2e/cypress/e2e/api/post/update-post.cy.js
@@ -4,7 +4,7 @@ describe("UPDATE Post - (PATCH) /api/post/:id", () => {
     const apiUrl = "http://localhost:9000";
 
     const testPost = {
-        accountImage: "https://avatars.githubusercontent.com/u/4129325?v=4",
+        accountImage: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwordpress.org%2Fplugins%2Fuser-avatar-reloaded%2F&psig=AOvVaw2nbyJIvY_c1K0Q-xucV4BL&ust=1715673395034000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNjHm8yTioYDFQAAAAAdAAAAABAE",
         accountName: "Test user",
         postDate: new Date().toDateString(),
         postText: "Test post API",
@@ -13,7 +13,7 @@ describe("UPDATE Post - (PATCH) /api/post/:id", () => {
     };
 
     const updatedPost = {
-        accountImage: "https://avatars.githubusercontent.com/u/4129325?v=4",
+        accountImage: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwordpress.org%2Fplugins%2Fuser-avatar-reloaded%2F&psig=AOvVaw2nbyJIvY_c1K0Q-xucV4BL&ust=1715673395034000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNjHm8yTioYDFQAAAAAdAAAAABAE",
         accountName: "Updated Test user",
         postDate: new Date().toDateString(),
         postText: "Test post API",
